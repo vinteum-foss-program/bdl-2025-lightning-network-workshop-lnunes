f2dd46715ee63496f8a2e0985dc5860ffa6ba519
Leandro Nunes <leandro.n@gmail.com>
2025-12-19 15:03:44 -0300

    Done replit lightning workshop

--- submissions/done.sh
+++ submissions/done.sh
@@ -1,6 +1,7 @@
 # When you finish the workshop:
 # 1. add a link to your replit fork in the next line
 #
+# https://replit.com/@leandron1/Programming-Lightning-Intro-to-Payment-Channels-mini
 #
 # 2. change the FAIL string to PASS.
-echo "FAIL"
+echo "PASS"
